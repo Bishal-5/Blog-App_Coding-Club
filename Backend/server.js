0103bf43e40884fba033737d9c771661323ac953
Bishal Naskar <naskarbishal514@gmail.com>
2025-03-23 13:25:08 +0530

    Initialize

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,15 @@
 import express from 'express';
 import dotenv from "dotenv";
+import cors from 'cors';
+
 const app = express();
+dotenv.config();
+
+// Middleware for data parsing and cors
+const corsOptoins = {
+    origin: true,
+    credentials: true
+}
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,6 +17,8 @@ app.get('/', (req, res) => {
     res.send('Hello!! Welcome To My Server...');
 })
 
+app.use(cors(corsOptoins)); // cors
+
 app.listen(PORT, () => {
-    console.log('Server is now live!!!');
+    console.log(`Server is running on port http://localhost:${PORT}`);
 })
