985051aa1949a6fb8432cbeae73e27f73875fab7
Bishal Naskar <naskarbishal514@gmail.com>
2025-03-23 23:27:13 +0530

    Second Update

--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,26 @@
 import express from 'express';
 import dotenv from "dotenv";
 import cors from 'cors';
+import db from './config/db.js';
+import UserRoutes from './routes/userAuth.js';
 
 const app = express();
-dotenv.config();
-
-// Middleware for data parsing and cors
-const corsOptoins = {
-    origin: true,
-    credentials: true
-}
-
+app.use(express.json()); // data parsing
 const PORT = process.env.PORT || 5000;
+dotenv.config(); // env variables
+db(); // database connection
+
+const corsOptoins = {
+    origin: true, 
+    credentials: true
+} // cors
 
 app.get('/', (req, res) => {
     res.send('Hello!! Welcome To My Server...');
 })
 
 app.use(cors(corsOptoins)); // cors
+app.use('/user', UserRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
